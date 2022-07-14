900ed8178168a6314b89af75f076d4c95870718a
Giorgio Scorzelli <scrgiorgio@gmail.com>
2022-07-14 10:10:09 +0000

    new changes:

--- README.md
+++ README.md
@@ -26,32 +26,24 @@ source venv/bin/activate
 python3 -m pip install -r requirements.txt
 ```
 
-
-
-Start the Flask server:
-- if you want to run locally, modify `app.py` to use `app.run(debug=True)`
-- if you want to run globally, modify `app.py` to have `app.run(host="0.0.0.0", port=4999, debug=True)`
-  - also: `sudo ufw allow 4999 && sudo ufw reload`
+Edit the `config.yaml` file and change as needed
 
 ```
 
-# this is getting your public ip
-export SERVER_IP=$(curl ifconfig.me)
+# do the first clone, change as needed
+git clone https://github.com/okoppe/Juypter-Notebook-Repo /tmp/nsdf-data-portal/Juypter-Notebook-Repo
 
-# choose a github repo with bokeh notebooks
-export GITHUB_REPO=https://github.com/okoppe/Juypter-Notebook-Repo
-
-# it will be cloned locally here
-export LOCAL_GITHUB_REPO=/tmp/nsdf-data-portal/$(echo $GITHUB_REPO | sed 's/https\?:\/\///')
-
-# do the first clone
-mkdir -p $(dirname $LOCAL_GITHUB_REPO)
-git clone ${GITHUB_REPO} ${LOCAL_GITHUB_REPO}
+# before running allow some port range, e.g
+sudo ufw allow 5000:6000/tcp
 
 # run the flask app
 cd flask_app
 source venv/bin/activate
-python3 app.py
+
+# this may be helpful to release old processes
+# sudo killall python3
+
+./run.sh
 ```
 
 You may be prometed to enter your sudo password.
--- flask_app/app.py
+++ flask_app/app.py
@@ -1,46 +1,182 @@
-from flask import Flask, render_template, request, Response, send_from_directory
-import os
+import os, logging, subprocess, threading, glob, signal, time, traceback,random, yaml
+from flask import Flask, render_template, request, Response, send_from_directory, redirect
 from multiprocessing import Process,Queue,Pipe
-from server_pipe_test import f
+import bokeh
 
-selectedValue2 = " "
+# global config
+config={}
+
+# flask app
 app = Flask(__name__)
+logger=app.logger
+
+# //////////////////////////////////////////////////////////////
+class Notebook:
+
+	# constructor
+	def __init__(self, key, filename, port):
+		'''
+		The Notebook class represents a single juypter notebook.
+		A Notebook object has all the information needed to server the notebook to a bokeh server.
+		'''
+		self.key = key
+		self.filename = filename
+		self.port = port
+		self.thread=None
+
+	# run
+	def run(self): 
+		'''
+		Serves the Bokeh application to the websocket. Starts the server on its on thread.
+		'''
+		def startServer(self):
+			subprocess.call(['python3', '-m', 'bokeh', 'serve',  self.filename, '--port', str(self.port), '--allow-websocket-origin='+config["ip"]+':'+str(self.port)])
+			
+		self.thread = threading.Thread(target=startServer, args=(self,))
+		self.thread.start()
+
+	# shutdown
+	def shutdown(self):
+		'''
+		kill the thread
+		'''
+		self.thread.kill()
+
+	# getUrl
+	def getUrl(self):
+		'''
+		Getter function to return the link to the Bokeh server.
+		'''
+		return ('http://'+config["ip"]+':'+str(self.port)+'/'+self.key.replace(".ipynb",""))
+
+
+
+# //////////////////////////////////////////////////////////////
+class Notebooks:
+
+	# constructor
+	def __init__(self):
+		'''
+		This class represents a colection of Notebook objects that corrispond to the .ipynb files in a git hub repo.
+		'''
+		self.map = {}
+
+	singleton=None
+
+	@staticmethod
+	def getSigleton():
+		if Notebooks.singleton is None:
+			singleton = Notebooks()
+			singleton.updateAll()
+			# singleton.trackChangesInBackground()
+			Notebooks.singleton=singleton
+		return Notebooks.singleton
+
+	# addNotebook
+	def addNotebook(self, key, filename):
+		if key in self.map: return
+		port = random.randint(config["worker-ports"]["from"],config["worker-ports"]["to"]) # TODO: here I am hoping for no collision
+		notebook = Notebook(key, filename , port)
+		notebook.run()
+		self.map[key] = notebook
+
+	# removeNotebook
+	def removeNotebook(self, key):
+		if key not in self.map: return
+		self.map[key].shutdown()
+		self.map.pop(key)
+
+	# updateAll
+	def updateAll(self):
+
+		files = [os.path.relpath(it,config["local"]) for it in glob.glob(os.path.join(config["local"] + "/**/*.ipynb"),recursive=True)]
+		logger.info(f"updateAll {config['local']} {files}")
+
+		# delete files and Notebook objects from the array that have been deleted in the repo
+		for key in self.map:
+			if key not in files:
+				self.removeNotebook(key)
+
+		# Create a new Notebook object for each new .ipynb file
+		for key in files:
+			self.addNotebook(key, os.path.join(config["local"],key))
+
+	# trackChangesInBackground
+	def trackChangesInBackground(self):
+		def RunInBackground():
+			while True:
+				try:
+					import git
+					g = git.cmd.Git(config["local"])
+					g.pull(config["remote"])
+					self.updateAll()
+				except Exception as ex:
+					logger.error(ex)
+		threading.Thread(target=self.RunInBackground, args=()).start()
+
+	# getLinks
+	def getLinks(self):
+		ret={}
+		for k,v in self.map.items():
+			ret[k]=v.getUrl()
+		return ret
+
+# //////////////////////////////////////////////////////////////////////////
+def HandleRequest(child_conn):
+	child_conn.send(Notebooks.getSigleton().getLinks())
+	child_conn.close()
+
+# //////////////////////////////////////////////////////////////////////////
+def GetLinks():
+	parent_conn,child_conn = Pipe()
+	p = Process(target=HandleRequest, args=(child_conn,))
+	p.start()
+	ret = parent_conn.recv()
+	p.join()
+	return ret
+
+
+selected=None
+
+# //////////////////////////////////////////////////////////////////////////
 #hone directory
 @app.route("/")
 def index():
-    parent_conn,child_conn = Pipe()
-    p = Process(target=f, args=(child_conn,))
-    p.start()
+	links=GetLinks()
+	return render_template(
+		"index.html",  
+		links=list(links.keys()), 
+		selected=None
+	) 
 
-    BokehLinkDictFlaskCopy = parent_conn.recv()
-    p.join()
-    return render_template("index.html", noteBookNames=list(BokehLinkDictFlaskCopy.keys()), BokehLinkDictFlaskCopy = BokehLinkDictFlaskCopy,
-        bool_files = len(BokehLinkDictFlaskCopy.keys()), selectedValue = "select a notebook") #len(BokehLinkDictFlaskCopy), selectedValue = list(BokehLinkDictFlaskCopy.keys())[0],
-        #linkToBokeh = BokehLinkDictFlask[selectedValue])
-
+# //////////////////////////////////////////////////////////////////////////
 #path for veiwing data set inline
-@app.route('/chooseDataSet/<noteBookName>', methods = ['POST', 'GET'])
-def chooseDataSet(noteBookName):
-    global selectedValue2
-    selectedValue2 = noteBookName #str(request.form.get('selected_Notebook'))
+@app.route('/chooseNotebook/<key>', methods = ['POST', 'GET'])
+def chooseNotebook(key):
+	links=GetLinks()
+	return redirect(links[key].replace(".ipynb",""), code=302)
 
-    parent_conn,child_conn = Pipe()
-    p = Process(target=f, args=(child_conn,))
-    p.start()
 
-    BokehLinkDictFlaskCopy2 = parent_conn.recv()
-    p.join()
-
-    return render_template("index.html", noteBookNames=list(BokehLinkDictFlaskCopy2.keys()),
-        bool_files = len(BokehLinkDictFlaskCopy2.keys()), selectedValue = selectedValue2,
-        linkToBokeh = BokehLinkDictFlaskCopy2[selectedValue2])
-
+# //////////////////////////////////////////////////////////////////////////
 #path for downloading file
-@app.route("/download", methods=['GET', 'POST'])
-def download():
+@app.route("/downloadNotebook", methods=['GET', 'POST'])
+def downloadNotebook():
+	notebooks=Notebooks.getSingleton()
+	return send_from_directory(
+		directory=config["local"], 
+		path=notebooks.selected, 
+		as_attachment=True)
 
-    return send_from_directory(directory="/home/owenkoppe/Juypter-Notebook-Repo", path=selectedValue2, as_attachment=True)
+# //////////////////////////////////////////////////////////////////////////
+def LoadConfigFile():
+	from yaml.loader import SafeLoader
+	with open(os.path.join(os.path.dirname(__file__),'config.yaml')) as f:
+		return yaml.load(f, Loader=SafeLoader)
+		
 
 # //////////////////////////////////////////////////////////////
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=4999, debug=True)
+	logger.setLevel(logging.INFO)
+	config=LoadConfigFile()
+	logger.info(f"Notebooks {config}")
+	app.run(host="0.0.0.0", port=config["port"], debug=bool(config["debug"]))
